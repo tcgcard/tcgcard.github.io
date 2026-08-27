7046980704f52f53246c483af00634a27c10421d
tcgcard <bryant66657@students.apsb.org>
2026-08-27 19:01:20 +0000

    Finalized platformer game

--- fsd-projects/platformer/platformer.js
+++ fsd-projects/platformer/platformer.js
@@ -34,20 +34,23 @@ $(function () {
     createPlatform(0, 700, 200, 20)
 createPlatform(500, 500, 10, 290)
  createPlatform(300, 600, 200, 20)
-createPlatform(600, 400, 200, 20)
+createPlatform(600, 300, 200, 20)
  createPlatform(200, 900, 20, 290)   
- createPlatform(500, 1300, 200, 20)  
-
-
-
-
+ createPlatform(1300, 500, 200, 20)
+ createPlatform(1000, 500, 200, 20)
+ createPlatform(900, 350, 200, 20)
+ createPlatform(700, 600, 200, 20)
+createPlatform(1100, 700, 200, 200)
+createPlatform(200, 400, 200, 20)
+  createBadPlatform(0,739,20000,20)
+  
 // TODO 3 - Create Collectables
-
+createCollectable("diamond", 1250, 600)
 
 
     
     // TODO 4 - Create Cannons
-createCannon("right", 300,1000)
+createCannon("right", 200,100)
 
 
     
